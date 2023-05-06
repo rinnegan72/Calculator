1c80b9c9fe8fad4aa63bcc9f2902ec114b4d8788
R Sriram <69243366+rinnegan72@users.noreply.github.com>
2023-05-06 12:22:22 +0530

    eslint warning fixed 🥳

--- client/src/components/Calculator.tsx
+++ client/src/components/Calculator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { create, all } from 'mathjs';
